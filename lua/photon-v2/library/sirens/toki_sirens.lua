941dd73e55b74c21e4fec2ee64cfa635ee00bff6
Tokimune <166393985+TokimuneFIN@users.noreply.github.com>
2024-04-29 19:02:00 +0300

    Added original files
    Original files added

---
+++ lua/photon-v2/library/sirens/toki_sirens.lua
@@ -0,0 +1,54 @@
+Photon2.RegisterSiren(
+	{
+		Name = "fs_vama420_toki", 
+		Make = "Federal Signal",
+		Model = "Vama AS-420",
+		Author = "Tokimune", -- Source (removed video from Youtube)
+		Sounds = {
+			["WAIL"] = { Sound = "photon/sirens/fs_vama420_toki/vama420_wail.wav", 	Default = "T1" },
+			["YELP"] = { Sound = "photon/sirens/fs_vama420_toki/vama420_yelp.wav", 	Default = "T2" },
+			["HILO"] = { Sound = "photon/sirens/fs_vama420_toki/vama420_hilo.wav", 	Default = "T3" },
+		}
+	}
+)
+
+Photon2.RegisterSiren(
+	{
+		Name = "ulvo_sos102_toki", 
+		Make = "Ulvo Sirra",
+		Model = " SOS-102",
+		Author = "Tokimune", -- Source (Bielsko112 on Youtube)
+		Sounds = {
+			["YELP"] = { Sound = "photon/sirens/ulvo_sos102_toki/us_sos102_yelp.wav", 	Default = "T1" },
+			["HILO"] = { Sound = "photon/sirens/ulvo_sos102_toki/us_sos102_hilo.wav", 	Default = "T2" },
+		}
+	}
+)
+
+Photon2.RegisterSiren(
+	{
+		Name = "becker_sinfo_toki", 
+		Make = "Becker Sinfo",
+		Model = "Becker Sinfo",
+		Author = "Tokimune", -- Source (112 Brandenburg on Youtube)
+		Sounds = {
+			["HILO"] = { Sound = "photon/sirens/becker_sinfo_toki/becker_sinfo_toki.wav", 	Default = "T1" },
+		}
+	}
+)
+
+Photon2.RegisterSiren(
+	{
+		Name = "fs_vama380_toki", 
+		Make = "Federal Signal",
+		Model = "Vama AS-380",
+		Author = "Tokimune", -- Source (maciek92 on Youtube)
+		Sounds = {
+			["WAIL"] = { Sound = "photon/sirens/fs_vama_380_toki/fs_vama380_wail.wav", 	Default = "T1" },
+			["YELP"] = { Sound = "photon/sirens/fs_vama_380_toki/fs_vama380_yelp1.wav", 	Default = "T2" },
+			["YELP2"] = { Sound = "photon/sirens/fs_vama_380_toki/fs_vama380_yelp2.wav", Default = "T3" },
+			["HILO"] = { Sound = "photon/sirens/fs_vama_380_toki/fs_vama380_hilo.wav", Default = "T4" },
+			["AIRHORN"]  = { Sound = "photon/sirens/fs_vama_380_toki/fs_vama380_air.wav", Default = "AIR", Label = "AIR" },
+		}
+	}
+)
